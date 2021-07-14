fa2ed90a6d7387b9f49eb3a34d080e20edbaab9e
tak-dwc <ec2-user@ip-172-31-34-75.ec2.internal>
2021-07-14 03:18:55 +0000

    [update]model book

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about" 
+  
+  resources :books,only: [:index,:create,:show,:edit,:update,:destroy]
 end
