bff199330ac76df1d68b0386cbf7df376de9a00a
tak-dwc <ec2-user@ip-172-31-34-75.ec2.internal>
2021-07-15 10:17:04 +0000

    [start]レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,15 @@ class BooksController < ApplicationController
     @books = Book.all
     @book=Book.new(book_params)
     @book.user_id= current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+   if @book.save
+      flash[:notice]="You have created book successfully."
+       redirect_to book_path(@book.id)
+   else
+       @user = current_user
+       render :index
+   end
   end
+
 
   def show
      @books = Book.find(params[:id])
@@ -27,15 +33,19 @@ class BooksController < ApplicationController
   
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
+   if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
+   else
+    render :edit
+   end
   end
   
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
     redirect_to books_path
-  end
+  end 
   
    private
 
