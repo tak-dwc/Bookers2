0b2b680e18953cf9cc2cc4b799bac9278bdc5d1c
tak-dwc <taku3277@gmail.com>
2021-07-25 09:39:55 +0000

    [Start]Add Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :books,dependent: :destroy  
+  has_many :books,dependent: :destroy
   attachment :profile_image
+  has_many :book_comments ,dependent: :destroy
   
   validates :name,length: { minimum: 2, maximum: 20 } ,uniqueness: true
   validates :introduction,length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about" 
   
-  resources :books,only: [:index,:create,:show,:edit,:update,:destroy]
   resources :users,only: [:index,:show,:edit,:update,]
+  
+  resources :books,only: [:index,:create,:show,:edit,:update,:destroy] do
+    resources :book_comments,only: [:create,:destroy]
+  end  
+  
 end
 
