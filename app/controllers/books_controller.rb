3e9ce21ac44f9d9a0d2c1ffe702fc4255282cfbb
tak-dwc <taku3277@gmail.com>
2021-07-30 03:15:36 +0000

    controller修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
-    @book = Book.new
+    @book = Book.all
+    @book_new = Book.new
     @user = current_user
-    
+
   end
-  
+
   def create
-    @books = Book.all
-    @book=Book.new(book_params)
-    @book.user_id= current_user.id
-   if @book.save
+    @book = Book.all
+    @book_new = Book.new(book_params)
+    @book_new.user_id= current_user.id
+   if @book_new.save
       flash[:notice]="You have created book successfully."
-       redirect_to book_path(@book.id)
+       redirect_to book_path(@book_new.id)
    else
        @user = current_user
        render :index
@@ -20,19 +20,19 @@ class BooksController < ApplicationController
   end
 
   def show
-     @books = Book.find(params[:id])
-     @user = @books.user
-     @book = Book.new
+     @book = Book.find(params[:id])
+     @user = @book.user
+     @book_new = Book.new
      @book_comment = BookComment.new
   end
 
-  def edit 
+  def edit
      @book = Book.find(params[:id])
     unless @book.user == current_user
-    redirect_to books_path    
+    redirect_to books_path
     end
-  end  
-  
+  end
+
   def update
     @book=Book.find(params[:id])
    if @book.update(book_params)
@@ -42,17 +42,17 @@ class BooksController < ApplicationController
     render :edit
    end
   end
-  
+
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
     redirect_to books_path
-  end 
-  
+  end
+
    private
 
   def book_params
     params.require(:book).permit(:title,:body,:user_id)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
-    @book = Book.new
-  
-  
+    @book_new = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @book_new = Book.new
   end
 
   def edit
