84fbd38cd28be592f1677e20559f9b7859941b1d
tak-dwc <taku3277@gmail.com>
2021-08-05 03:00:11 +0000

    [start]8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :book_comments ,dependent: :destroy
   has_many :favorites ,dependent: :destroy
-  
   has_many :favorite_books, through: :favorites, source: :book
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
@@ -16,6 +15,9 @@ class User < ApplicationRecord
 
   has_many :re_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :re_relationships, source: :follower
+  
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
  
   def follow(user_id)
     relationships.create(followed_id: user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   
   end  
   
+  resources :messages,only: [:create]
+  resources :rooms,only: [:create,:show]
+  
   resources :books,only: [:index,:create,:show,:edit,:update,:destroy] do
     resource :favorites ,only:[:create, :destroy]
     
