85204a8079448d1b2d5c7f5ceb15f7555167959f
tak-dwc <taku3277@gmail.com>
2021-08-07 09:42:20 +0000

    [finish]7c

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about" 
   get "/search" => "searches#search"
   
-  resources :users,only: [:index,:show,:edit,:update,] do 
+  resources :users,only: [:index,:show,:edit,:update] do 
     resource :relationships, only: [:create,:destroy]
    get 'followings' => 'relationships#followings',as: 'followings'
    get 'followers' => 'relationships#followers' ,as: 'followers'
