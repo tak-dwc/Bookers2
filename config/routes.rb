b64f09ff4ff03582efa738d4348e3b976061b1db
tak-dwc <taku3277@gmail.com>
2021-08-08 08:17:39 +0000

    [finish]9c

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
    end  
   resources :groups do
    get "join" => "groups#join"
+   get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   
   resources :messages,only: [:create]
